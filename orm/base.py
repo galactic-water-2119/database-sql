c8b201f1f92dcc43ee144a5c7aa7a73e78eed236
Tejesh Agrawal <tejeshagrawal@gmail.com>
2019-02-04 19:03:21 +0530

    Update base.py

--- orm/base.py
+++ orm/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://tejeshagrawal:xdr5$ESZ@localhost:5432/sqlalchemy')
+engine = create_engine('postgresql://username:password@localhost:5432/db_name')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
