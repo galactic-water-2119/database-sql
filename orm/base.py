86128273e9fb4ba5ccfc35c1b13796085f6f8c28
Tejesh Agrawal <tejeshagrawal@gmail.com>
2019-02-05 20:44:04 +0530

    Update base.py

--- orm/base.py
+++ orm/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://username:password@localhost:5432/db_name')
+engine = create_engine('postgresql://username:password@localhost:5432/db_name', pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
